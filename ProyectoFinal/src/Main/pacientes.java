de419f659e40775495648687b59679e260248532
jobyfarra <jojofaso96@gmail.com>
2021-01-13 22:30:06 -0500

    Modificando funcion CargarDatos

--- ProyectoFinal/src/Main/pacientes.java
+++ ProyectoFinal/src/Main/pacientes.java
@@ -24,36 +24,35 @@ public class pacientes {
          private Map <String,ArrayList<Integer>> dataset = new LinkedHashMap<String,ArrayList<Integer>>();
          private Map <String,Double> Gini = new LinkedHashMap <String,Double> ();
          
-         public void  CargarDatos(String nombreA) {
+         public static pacientes  CargarDatos(String nombreA) {
+             pacientes p1= new pacientes();
              try {
                  BufferedReader  br =new BufferedReader(new FileReader(nombreA));
                  String encabezado= br.readLine();//se captura el headline de los atributos
                  String[] atributos=encabezado.split(",");
                  for (String x: atributos){
-                    dataset.put( x ,new ArrayList <Integer>());
-                    Gini.put(x, 0.0);
+                    p1.dataset.put( x ,new ArrayList <Integer>());
+                    p1.Gini.put(x, 0.0);
                  }
                 String linea = br.readLine();
                  while(linea!=null){//se lee las 299 entradas del archivo
                      String[] datos =linea.split(",");
                      for(int j=0;j<datos.length;j++){
-                        dataset.get(atributos[j]).add(Integer.parseInt(datos[j]));//se cargan las ArrayLists con los valores de las entradas
+                        p1.dataset.get(atributos[j]).add(Integer.parseInt(datos[j]));//se cargan las ArrayLists con los valores de las entradas
                      }
                     linea=br.readLine();
-                 }
-                 
+                 }                 
              } catch (Exception ex) {
-                 
-             }
-         
-         
-         
+                 System.out.println("No se encontro el Archivo");  
+             }        
+             return p1;
          }
   
     public static void main (String [] args){
-         pacientes pc=new pacientes();
+         pacientes pc= new pacientes();
+         pc=pc.CargarDatos("D:\\Trabajos\\ESPOL\\Estructuras\\ProyectoFinal\\ProyectoED_Par3\\ProyectoFinal\\src\\Files\\pacientes1.csv");
          //Se debe cambiar el path del archivo, ya que se encuentra en otra carpeta
-         pc.CargarDatos("D:\\Trabajos\\ESPOL\\Estructuras\\ProyectoFinal\\ProyectoED_Par3\\ProyectoFinal\\src\\Files\\pacientes1.csv");
+         
          System.out.println("Diccionario DataSet");
          for (Map.Entry entry : pc.dataset.entrySet()) {
             System.out.println("Clave : " + entry.getKey()
