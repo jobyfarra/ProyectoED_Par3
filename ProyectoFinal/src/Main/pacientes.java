bdd9f907b529b518dee38871ec919d5e2675b014
jobyfarra <jojofaso96@gmail.com>
2021-01-14 16:09:05 -0500

    Agregando codigo

--- ProyectoFinal/src/Main/pacientes.java
+++ ProyectoFinal/src/Main/pacientes.java
@@ -47,19 +47,88 @@ public class pacientes {
              }        
              return p1;
          }
+         
+         private double obtenerGini(String Atributo, String Target){
+                double gn;
+                int ss=0;
+                int sn=0;
+                int ns=0;
+                int nn=0;
+                ArrayList<Integer> atri=this.dataset.get(Atributo);
+                ArrayList<Integer> trgt=this.dataset.get(Target);
+                
+                if (Atributo!=Target){
+                    for(int j=0;j<atri.size();j++){                      
+                        switch (atri.get(j)){
+                            case 1:
+                                switch(trgt.get(j)){
+                                    case 1:
+                                        ss++;
+                                        break;
+                                    case 0:
+                                        sn++;
+                                        break;
+                                
+                                }
+                                break;
+                            case 0:
+                                switch(trgt.get(j)){
+                                    case 1:
+                                        ns++;
+                                        break;
+                                    case 0:
+                                        nn++;
+                                        break;                               
+                                }
+                                break;                               
+                        } 
+                    }
+                    double gn1=1-Math.pow(ss/(ss+sn),2)-Math.pow(sn/(ss+sn),2);
+                    System.out.println(gn1);
+                    double gn2=1-Math.pow(ns/(ns+nn),2)-Math.pow(nn/(ns+nn),2);
+                    System.out.println(gn2);
+                    gn=((ss+sn)/atri.size())*gn1+((ns+nn)/atri.size())*gn2;
+                    
+                
+                }else{
+                    for(int i: atri){
+                        if (i==1){
+                            ss++;
+                        }else{
+                            nn++;
+                        }
+                    
+                    }
+                    
+                    gn=1-Math.pow(ss/(atri.size()),2)-Math.pow(nn/atri.size(),2);
+                    System.out.println();
+                
+                }
+                
+                
+                
+                return gn;
+         
+         }
+         
+         public void  actGini(String Atributo){
+         
+         
+         }
   
     public static void main (String [] args){
-         pacientes pc= pacientes.CargarDatos("D:\\Trabajos\\ESPOL\\Estructuras\\ProyectoFinal\\ProyectoED_Par3\\ProyectoFinal\\src\\Files\\pacientes1.csv");
+         pacientes pc= pacientes.CargarDatos("src/Files/pacientes1.csv");
          //Se debe cambiar el path del archivo, ya que se encuentra en otra carpeta
          
-         System.out.println("Diccionario DataSet");
-         for (Map.Entry entry : pc.dataset.entrySet()) {
-            System.out.println("Clave : " + entry.getKey()
-                    +" ->"+ " Valor : " + entry.getValue()+"->"+" Dim : "+ ((ArrayList)entry.getValue()).size());
-        }System.out.println("Diccionario Gini");
-         for (Map.Entry entry : pc.Gini.entrySet()) {
-            System.out.println("Clave : " + entry.getKey()
-                    +" ->"+ " Valor : " + entry.getValue());
-                    }
+//         System.out.println("Diccionario DataSet");
+//         for (Map.Entry entry : pc.dataset.entrySet()) {
+//            System.out.println("Clave : " + entry.getKey()
+//                    +" ->"+ " Valor : " + entry.getValue()+"->"+" Dim : "+ ((ArrayList)entry.getValue()).size());
+//        }System.out.println("Diccionario Gini");
+//         for (Map.Entry entry : pc.Gini.entrySet()) {
+//            System.out.println("Clave : " + entry.getKey()
+//                    +" ->"+ " Valor : " + entry.getValue());
+//                    }
+        System.out.println(pc.obtenerGini("anemia", "anemia"));
     }
 }
